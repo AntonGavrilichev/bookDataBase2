2e0cf2a1a95f5519bea00017cfa90ca1606b4dd7
AntonGavrilichev <antongavrilicev903@gmail.com>
2024-06-17 10:57:00 +0300

    Последняя версия

---
+++ src/main/java/ru/BookDB/repositories/PeopleRepository.java
@@ -0,0 +1,13 @@
+package ru.BookDB.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.BookDB.models.Person;
+
+import java.util.Optional;
+
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByFullName(String fullName);
+}
